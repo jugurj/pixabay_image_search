45ca7458678b296bb9b98e11b20d93761d683e80
Yuri Gurjev <juriyunna27@gmail.com>
2019-02-13 15:24:53 +0200

    Fixed bug when images showed up with empty keyword after searching and deleting it.

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -15,10 +15,15 @@ class Search extends Component {
     }
 
     onKeyChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value }, () => {
-            axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchKey}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
-                .then(res => this.setState({ images: res.data.hits }))
-                .catch(err => console.log(err));
+        const val = e.target.value;
+        this.setState({ [e.target.name]: val }, () => {
+            if (val === '') {
+                this.setState({ images: [] })
+            } else {
+                axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchKey}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
+                    .then(res => this.setState({ images: res.data.hits }))
+                    .catch(err => console.log(err));
+            }
         });
     }
 
